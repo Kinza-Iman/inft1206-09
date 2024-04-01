8c819f0c85f53943b8d32ba758dfa98e6c0ab123
Kinza-Iman <156018753+Kinza-Iman@users.noreply.github.com>
2024-04-01 17:37:28 -0400

    looping thru images

--- assignment4/part2/gallery-start/main.js
+++ assignment4/part2/gallery-start/main.js
@@ -6,15 +6,40 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-var images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 
-/* Looping through images */
+const altText = {'pic1.jpg' : 'human eye',
+'pic2.jpg' : 'round rock',
+'pic3.jpg' : 'pretty purple flowers',
+'pic4.jpg' : 'egyptian painting',
+'pic5.jpg' : 'butterfly on a leaf'}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+/* Looping through images */
+for (let i = 0; i < imageArray.length; i++)
+{
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src',  'images/' + imageArray[i]);
+    newImage.setAttribute('alt', altText[i]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', (e) => 
+    {
+        displayedImage.src = e.target.src;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => 
+{
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') 
+    {
+    
+    } else 
+    
+    {
+        
+    }
+});
